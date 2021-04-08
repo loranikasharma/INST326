5ca92f66b37f74686cded96012535e7a1a2f06bd
jpatel369 <jayapatel369@gmail.com>
2021-04-08 15:18:17 -0400

    Team.py docs

--- Team.py
+++ Team.py
@@ -3,6 +3,9 @@
 #if statements
 import pandas as pd
 
+df = pd.read_csv("INST326 FINAL_Presidents.csv")
+print df
+
 class User:
     """
     This class keeps track of the users attempts, name and score
@@ -12,46 +15,66 @@ class User:
         attempts(int): Number of attempts
         score(int): The score of the user
     """
-    def __init__(self,name,attempts,score):
+    def __init__(self,name):
         """
-        Initializes a user object
+        Initializes a user object and creates attempts attributes and score attribute
         
         Args:
             name(String): Name of user
-            attempts(int): Number of attempts
-            score(int): The score of the user
         
         Side effects:
             Initializes values
         """
-    
-
-class President:
-    
-     def __init__(self,num):
-    
-    def file_opener():
-    def president_name():
-        
-    def one_great_act():
-    
 
 
 class Game:
     
-    def __init__():
+    def __init__(self):
         
         
-    def guess():
-        
-        
-    def compare():
-        
-        
-    def reverse():
-            
+    def guess(self,ser):
+    """
+    Is the guessing game
     
-    def score():
+    Attributes:
+        ser(series): A column from the file
+    Side effects:
+        Increases score based on answer given
+    """
         
+        
+    def compare(self,ser1,ser2):
+        """
+        This class keeps track of the users attempts, name and score
+    
+        Attributes:
+            ser1(series): Name of user
+            der2(series): Number of attempts
+        Side effects:
+            Prints out information regarding the presidents
+        """
+        
+    def reverse(self,data_frame):
+        """
+        Allows the user to put in information about a president and get the president
+        name.
+        
+        Attributes:
+            data_frame(data frame):The data frame consisting of all the presidents 
+        Side effects:
+            Prints out the president the user is specifing
+        """
+    
+    def score(self,score,name):
+        """
+        Keeps track of all the scores of all players who haver ever played and
+        updates the users specific score
+        
+        Attributes:
+            name (string)
+            score (int)
+        
+        Side effects:
+        """
         
     
