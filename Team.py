24a1d92d1cf29570af6baf955f646f60fb035e2c
jpatel369 <jayapatel369@gmail.com>
2021-04-08 14:44:31 -0400

    docstring changes

--- Team.py
+++ Team.py
@@ -1,2 +1,57 @@
-Team
-Loranika Sharma
+
+#pandas
+#if statements
+import pandas as pd
+
+class User:
+    """
+    This class keeps track of the users attempts, name and score
+    
+    Attributes:
+        name(String): Name of user
+        attempts(int): Number of attempts
+        score(int): The score of the user
+    """
+    def __init__(self,name,attempts,score):
+        """
+        Initializes a user object
+        
+        Args:
+            name(String): Name of user
+            attempts(int): Number of attempts
+            score(int): The score of the user
+        
+        Side effects:
+            Initializes values
+        """
+    
+
+class President:
+    
+     def __init__(self,num):
+    
+    def file_opener():
+    def president_name():
+        
+    def one_great_act():
+    
+
+
+class Game:
+    
+    def __init__():
+        
+        
+    def guess():
+        
+        
+    def compare():
+        
+        
+    def reverse():
+            
+    
+    def score():
+        
+        
+    
