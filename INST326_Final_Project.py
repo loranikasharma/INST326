069b12c6fff2bd61cd3307c9af68f132afea389f
Chris Marroquin <cmarro14@umd.edu>
2021-04-21 09:54:12 -0400

    Added somethings to main and completed my init method

--- INST326_Final_Project.py
+++ INST326_Final_Project.py
@@ -9,6 +9,7 @@ incorrect answer).
 # Final Project
 
 import pandas as pd
+from random import random
 
 #Replace comment with a random number generator
 
@@ -21,7 +22,7 @@ class User:
         attempts(int): Number of attempts
         score(int): The score of the user
     """
-    def __init__(self,name):
+    def __init__(self,name,score=0):
         """
         Initializes a user object and creates attempts attributes and a score attribute
         
@@ -62,6 +63,7 @@ class Game:
         Side effects:
             Initializes values
         """
+        self.name = name
         
     def guess(self,ser,player):
         """
@@ -117,8 +119,35 @@ def main():
     user instance will be initiated. A file will be created where the users score
     will be tracked. This is also where all of the funcitons/methods will be ran.
     """
-    #df = pd.read_csv("INST326 FINAL_Presidents.csv")
-    #print df
+    df = pd.read_csv("INST326 FINAL_Presidents.csv",index_col="Number President")
+    print("Hello! Welcome to the game featuring all of the presidents of the United\
+          States! We here at Presidents INC are happy you cam to play. Before we\
+          get started please enter your full name and a name you would like to give\
+          to the game your about to play.")
+    name = input("Please enter your name: ")
+    game_name = input("Please enter the name you wish to call this game: ")
+    game1 = Game(game_name)
+    player = User(name,0)
+    print("Now that you have entered the information. It is time for you to pick\
+        the game you want to play. The first is a guessing game where information of a \
+        random president will be given to you and you will have 3 guesses to guess\
+        the correct president. The second option is an interactive expericen where\
+        you will get to choose 2 presidents of the US and see how they differ. The third\
+        choice is when you get to give the information fo a president and we guess\
+        the president you are talking about.")
+    game_choice = input("Please type 1 for option 1, 2 for option 2 or 3 for option 3")
+    
+    while 0>= game_choice or game_choice >3:
+         game_choice = input("Please type 1 for option 1, 2 for option 2 or 3 for option 3")
+    
+    if game_choice == 1:
+        ran = random.randint(1,46)
+        game1.guess(df.loc[ran],player)
+    elif game_choice == 2:
+        
+    elif game_choice == 3:
+        
+    
     def score_board(file):
         """
         Prints out the leader of board
