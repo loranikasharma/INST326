79d1185383418c493a3e38777283d3cbc65dc0e2
Chris Marroquin <cmarro14@umd.edu>
2021-04-21 16:53:46 -0400

    Finished my part, made the parse args funciton, and added things to main which you should all take a look at.

--- INST326_Final_Project.py
+++ INST326_Final_Project.py
@@ -9,7 +9,7 @@ incorrect answer).
 # Final Project
 
 import pandas as pd
-from random import random
+import random
 
 #Replace comment with a random number generator
 
@@ -22,16 +22,17 @@ class User:
         attempts(int): Number of attempts
         score(int): The score of the user
     """
-    def __init__(self,name,score=0):
+    def __init__(self,name,score):
         """
         Initializes a user object and creates attempts attributes and a score attribute
         
         Args:
             name(String): Name of user
-        
+            score (int): The number of points the user starts off with by default
         Side effects:
             Initializes values
         """
+    
     
     def get_score(self):
         """
@@ -65,17 +66,39 @@ class Game:
         """
         self.name = name
         
-    def guess(self,ser,player):
+    def guess(self,df,player):
         """
         Is the guessing game
         
         Attributes:
-            ser(series): A President name column from the file (exaplain stucture of DF)
+            df (data frame): The data frame with information on the presidents
             player (user): Is a user object
         Side effects:
-            Increases score based on correct answer given.
+            Decreases score based on correct answer given.
         """
         
+        print(f"Hello {player.name}. Welcome to the guessing game. The max number\
+        of points you can get is 10 points. For every wrong answer 2 points will be subtracted. \
+        Once you reach 0 you lose and the correct answer will be shown. Good luck!")
+
+        ran = random.randint(1,46)
+        president = df.loc[ran]
+        l = [1,3,4,7,8,9]
+
+        while player.score >0:
+            ran_col = random.choice(l)
+            l.remove(ran_col)
+            print(f"{df.columns[ran_col]}: {president.iloc[ran_col]}")
+            guess = input("Who is the President?:")
+            if guess.lower() == president.loc["Name of President"].lower():
+                break
+            else:
+                player.score -= 2
+            
+        if score > 0:
+            print(f"Congrats! You won with a score of {score}!")
+        else:
+            print("Sadly you lost! Better luck next time")
         
     def compare(self,ser1,ser2):
         """
@@ -101,7 +124,7 @@ class Game:
     
     def score(self,score,name,file):
         """
-        Keeps track of all the scores of all players who haver ever played and
+        Keeps track of all the scores of all players who have ever played and
         updates the users specific score
         
         Attributes:
@@ -119,15 +142,19 @@ def main():
     user instance will be initiated. A file will be created where the users score
     will be tracked. This is also where all of the funcitons/methods will be ran.
     """
-    df = pd.read_csv("INST326 FINAL_Presidents.csv",index_col="Number President")
+    
+    scoreboard_file = open("ScoreBoard.txt","w+")
+    df = pd.read_csv("Inst326_Presidents_Info.csv",index_col="Number President")
+    
     print("Hello! Welcome to the game featuring all of the presidents of the United\
           States! We here at Presidents INC are happy you cam to play. Before we\
           get started please enter your full name and a name you would like to give\
           to the game your about to play.")
+    
     name = input("Please enter your name: ")
-    game_name = input("Please enter the name you wish to call this game: ")
+    game_name = input("Please enter the name you wish to call this trial: ")
     game1 = Game(game_name)
-    player = User(name,0)
+    player = User(name,10)
     print("Now that you have entered the information. It is time for you to pick\
         the game you want to play. The first is a guessing game where information of a \
         random president will be given to you and you will have 3 guesses to guess\
@@ -135,18 +162,23 @@ def main():
         you will get to choose 2 presidents of the US and see how they differ. The third\
         choice is when you get to give the information fo a president and we guess\
         the president you are talking about.")
-    game_choice = input("Please type 1 for option 1, 2 for option 2 or 3 for option 3")
     
-    while 0>= game_choice or game_choice >3:
+    game_choice = input("Please type 1 for option 1, 2 for option 2 or 3 for option 3")
+    again = 1
+    while game_choice <= 0 or game_choice > 3:
          game_choice = input("Please type 1 for option 1, 2 for option 2 or 3 for option 3")
     
-    if game_choice == 1:
-        ran = random.randint(1,46)
-        game1.guess(df.loc[ran],player)
-    elif game_choice == 2:
+    while again == 1:
+        if game_choice == 1:
+            game1.guess(df,player)
+            game1.score(player.score,player.name,scoreboard_file)
+        elif game_choice == 2:
+            
+        elif game_choice == 3:
         
-    elif game_choice == 3:
-        
+        again = input("Would you like to play again? Type 1 for yes or 0 for no: ")
+    
+    scoreboard_file.close()
     
     def score_board(file):
         """
@@ -158,8 +190,20 @@ def main():
             prints out the leader boards
         """        
 
-#Replace this comment with parse_args function
+def parse_args(arglist):
+    """Parse command-line arguments.
+    
+    one arguments is expected: the path to a CSV file
+    
+    Args:
+        arglist (list of str): command-line arguments.
+    
+    Returns:
+        namespace: an object with attributes csv_file
+    """
+    parser = ArgumentParser()
+    parser.add_argument("csv_file", help="path to life expectancy CSV file")
+    return parser.parse_args(arglist)
 
 if __name__ == "__main__":
-    """
-    """
+    main()
