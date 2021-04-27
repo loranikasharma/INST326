1824c1bdec4fbe225fd0dc49cf32d0467a04e58a
Chris Marroquin <cmarro14@umd.edu>
2021-04-27 16:30:04 -0400

    Testing

--- Team.py
+++ Team.py
@@ -17,7 +17,7 @@ class User:
     """
     def __init__(self,name):
         """
-        Initializes a user object and creates attempts attributes and score attribute
+        Initializes a user object and creates attempts attributes and a score attribute
         
         Args:
             name(String): Name of user
@@ -25,21 +25,37 @@ class User:
         Side effects:
             Initializes values
         """
+    
+    def get_score(self):
+        """
+        Allows the user see what their score is.
+        
+        Side effects:
+            prints out the score of the user.
+        """
 
 
 class Game:
     
-    def __init__(self):
+    def __init__(self,name):
+        """
+       Initializes the name of the game.
+        Args:
+            name(String): Name of game
         
+        Side effects:
+            Initializes values
+        """
         
-    def guess(self,ser):
+    def guess(self,ser,player):
     """
     Is the guessing game
     
     Attributes:
         ser(series): A column from the file
+        player (user): Is a user object
     Side effects:
-        Increases score based on answer given
+        Increases score based on answer given.
     """
         
         
@@ -75,6 +91,12 @@ class Game:
             score (int)
         
         Side effects:
+            prints out the score of everyone who has played.
         """
         
-    
+def main():
+    """
+    In the main function the CSV file will be opened. The game instacne and the 
+    user instance will be initiated. A file will be created where the users score
+    will be tracked. This is also where all of the funcitons/methods will be ran.
+    """
